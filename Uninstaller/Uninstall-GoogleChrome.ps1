e3ceda8d772fa0643676d97d139f3b885e5d2712
Christopher Dalyan Sparrowgrove <nam2long@users.noreply.github.com>
2022-09-09 05:08:32 -0500

    test upload
    test upload

---
+++ Uninstaller/Uninstall-GoogleChrome.ps1
@@ -0,0 +1,5 @@
+#Author: Christopher Sparrowgrove | Owner | CTO
+#Company: IT Ninjas Technology, LLC
+#Tool_Name: Ninja Deploy - Uninstaller
+#Script_Name: Ninja Deploy Uninstaller - Google Chrome
+#Desc: Silent Software Uninstaller for Google Chrome
